a5de5837fddb9f24632647fa61fa160065f70dd5
Topan Setiawan <topan.sosial@gmail.com>
2026-06-03 10:40:32 +0700

    Fix Bandit warnings

--- test_app.py
+++ test_app.py
@@ -4,4 +4,4 @@ from app import app
 def test_home():
     client = app.test_client()
     response = client.get("/")
-    assert response.status_code == 200
+    assert response.status_code == 200  # nosec B101
