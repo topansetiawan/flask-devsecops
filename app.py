f4037a5e2ddf038a651e5e76176d22b105804cd3
Topan Setiawan <topan.sosial@gmail.com>
2026-06-05 10:57:09 +0700

    Tambah file Service

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask
+from services import get_message
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello Jenkins CI/CD Flask 🚀"
+    return get_message()
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
--- test_app.py
+++ test_app.py
@@ -1,8 +1,9 @@
 from app import app
+from services import get_message
 
 def test_home():
     client = app.test_client()
     response = client.get("/")
 
-    # DIPAKSA SALAH
-    assert response.status_code == 500
+    assert response.status_code == 200
+    assert response.data.decode() == get_message()
